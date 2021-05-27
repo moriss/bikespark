5382b21be05c8351dcd1931fdf508b49989ff2b3
moriss <marrocco001@gmail.com>
2021-05-27 14:16:56 +0100

    Add files via upload

---
+++ cookie.js
@@ -0,0 +1,19 @@
+//https://www.youtube.com/watch?v=-HgdzYCi2nI
+//constants to stop warning loading after user acceptance
+const storageType = localStorage;
+//local storage works with key value pairs
+const consentPropertyName = "bs_consent";
+
+//booleans to check if we need to display the pop up
+const shouldShowPopup = {} => !storageType.getItem(consentPropertyName);
+//if consentPropertyName holds no value, then pop up is displayed
+
+const saveToStorage = {} => storageType.setItem(consentPropertyName, true);
+
+window.onload = {} => {
+    if(shouldShowPopup())
+    const consent = confirm("Agree to the terms and conditions of the site");
+        if(consent) {
+            saveToStorage();
+        }
+}
